558be2998a9da178b77cc7159b7e015e294df998
dxl <qiu@gmail.com>
2025-02-28 16:20:12 +0800

    是

--- dragon-daily-learning/src/main/java/com/example/dragondailylearning/DragonDailyLearningApplication.java
+++ dragon-daily-learning/src/main/java/com/example/dragondailylearning/DragonDailyLearningApplication.java
@@ -1,4 +1,4 @@
-package com.dxl.dragondailylearning;
+package com.example.dragondailylearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- dragon-daily-learning/src/test/java/com/example/dragondailylearning/DragonDailyLearningApplicationTests.java
+++ dragon-daily-learning/src/test/java/com/example/dragondailylearning/DragonDailyLearningApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dxl.dragondailylearning;
+package com.example.dragondailylearning;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
